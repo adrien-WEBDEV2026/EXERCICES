2b67e4127c59e40a90e3986f367b3ee7bb8e3ede
AdrienBct <a.bichart@yahoo.fr>
2026-02-11 14:20:14 +0100

    update correction

--- cours_1/staticWebSite/public/js/menu.js
+++ cours_1/staticWebSite/public/js/menu.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const menu = document.getElementById('menu');
 
     menuBtn.addEventListener('click', function() {
-        menuFlex.classList.toggle('menu--open');
+        menuFlex.classList.toggle('menu-open');
         menu.classList.toggle('menu-col')
     });
 });
